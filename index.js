b12c225e9b811ef066ea23810df9a1ad1620f791
“Genaro <ramoon1996@hotmail.com>
2021-04-23 13:48:37 +0200

    Iteration 2 done Create a Recipe

--- index.js
+++ index.js
@@ -14,13 +14,35 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
-  })
-  .then(() => {
+  // .then(self => {
+  //   console.log(`Connected to the database: "${self.connection.name}"`);
+  //   // Before adding any documents to the database, let's delete all previous entries
+  //   return self.connection.dropDatabase();
+  // })
+  .then((result) => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Asian Glazed Chicken Thighs",
+      level: "Amateur Chef",
+      ingredients: [
+      "1/2 cup rice vinegar",
+      "5 tablespoons honey",
+      "1/3 cup soy sauce (such as Silver Swan®)",
+      "1/4 cup Asian (toasted) sesame oil",
+      "3 tablespoons Asian chili garlic sauce",
+      "3 tablespoons minced garlic",
+      "salt to taste",
+      "8 skinless, boneless chicken thighs"
+    ],
+    cuisine: "Asian",
+    dishType: "main_course",
+    image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    duration: 40,
+    creator: "Chef LePapu"
+    })
+    .then((result)=>{
+      return console.log(result.title)
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
